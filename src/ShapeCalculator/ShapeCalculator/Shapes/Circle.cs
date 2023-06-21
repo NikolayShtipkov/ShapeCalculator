60ba2f31fa4d06c5f2986f0e0a720d7c0becf47f
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2023-06-21 11:15:36 +0300

    implement circle calculations

--- src/ShapeCalculator/ShapeCalculator/Shapes/Circle.cs
+++ src/ShapeCalculator/ShapeCalculator/Shapes/Circle.cs
@@ -2,5 +2,25 @@
 {
     public class Circle
     {
+        private readonly double _radius;
+
+        public Circle(double radius) 
+        {
+            _radius = radius;
+        }
+
+        public double GetArea()
+        {
+            double area = Math.PI * _radius * _radius;
+
+            return area;
+        }
+
+        public double GetCircumference()
+        {
+            double circumference = 2 * Math.PI * _radius;
+
+            return circumference;
+        }
     }
 }
--- src/ShapeCalculator/ShapeCalculator/ViewMenu.cs
+++ src/ShapeCalculator/ShapeCalculator/ViewMenu.cs
@@ -1,4 +1,6 @@
-﻿namespace ShapeCalculator
+﻿using ShapeCalculator.Shapes;
+
+namespace ShapeCalculator
 {
     public class ViewMenu
     {
@@ -10,13 +12,16 @@
             switch(choice)
             {
                 case "1":
-                    return true;
+                    CircleInput();
+                    return false;
                 case "2":
-                    return true;
+                    return false;
                 case "3":
+                    return false;
+                case "4":
                     return true;
                 default:
-                    Console.WriteLine("Unknown Command");
+                    Console.WriteLine("Unknown Command\n");
                     return false;
             }
         }
@@ -27,7 +32,34 @@
 
             Console.WriteLine("1. Circle");
             Console.WriteLine("2. Square");
-            Console.WriteLine("1. Triangle");
+            Console.WriteLine("3. Triangle");
+            Console.WriteLine("4. Exit\n");
+        }
+
+        private bool CircleInput()
+        {
+            Console.Write("What is the radius: ");
+            var input = Console.ReadLine();
+            Console.WriteLine();
+
+            double radius = 0;
+
+            bool isNumber = double.TryParse(input, out radius);
+            if (!isNumber)
+            {
+                Console.WriteLine("Invalid Input\n");
+
+                return true;
+            }
+
+            Circle circle = new Circle(radius);
+            double area = circle.GetArea();
+            double circumference = circle.GetCircumference();
+
+            Console.WriteLine("Area: " + Math.Round(area, 2));
+            Console.WriteLine("Circumference: " + Math.Round(circumference, 2));
+
+            return true;
         }
     }
 }
